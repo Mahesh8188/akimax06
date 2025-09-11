76dd051d2ce2b51ac74ca22e77097a1274935c90
Mahesh8188 <ddbambhava8182@gmail.com>
2025-09-11 08:20:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -162,7 +162,7 @@ LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
 PROTECT_CONTENT = is_enabled("PROTECT_CONTENT", True)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 LINK_MODE = is_enabled("LINK_MODE", True)
-TMDB_API_KEY = environ.get("TMDB_API_KEY", "")
+TMDB_API_KEY = environ.get("TMDB_API_KEY", "cbea51d46583c6e441be0e8df232cf07")
 
 # Online Streaming And Download
 STREAM_MODE = bool(environ.get("STREAM_MODE", False))  # Set True or Flase
